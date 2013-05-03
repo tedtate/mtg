209460a6f77ed46f0133499a26cdf7a835ed9019
Ted Tate <tedtate@gmail.com>
2013-05-02 21:34:08 -0700

    Add more card properties.
    * Add scrape debug flag

--- serve/scrape.js
+++ serve/scrape.js
@@ -8,12 +8,21 @@ exports.card = function(num, cb){
         if(err) return cb(null)
 
         var $ = window.$
-        var details = $('.cardDetails .smallGreyMono:nth-child(2)')
+        var details = $('.cardDetails .smallGreyMono:nth-child(2)'),
+            pt = details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ptRow .value').text().split('/');
         
         card = {
-            'cardname':  details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow .value').text().trim(),
-            'cmc':   details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow .value').text().trim(),
-            'type':   details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_typeRow .value').text().trim()
+            'cardname': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow .value').text().trim(),
+            'cmc': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cmcRow .value').text().trim(),
+            'type': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_typeRow .value').text().trim(),
+            'power': (pt[0] || '').trim(),
+            'toughness': (pt[1] || '').trim(),
+            'rules': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_textRow .cardtextbox').text().trim(),
+            'flavor': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_flavorRow .value').text().trim(),
+            'set': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_setRow .value').text().trim(),
+            'rarity': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_rarityRow .value').text().trim(),
+            'cardnumber': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_numberRow .value').text().trim(),
+            'artist': details.find('#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_artistRow .value').text().trim()
         }
 
         cb(card)
--- serve/server.js
+++ serve/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = 8001
 var db = require("./db.js")
+var SCRAPE_DEBUG = true
 
 app.get("/", function(req, rsp){
   rsp.send("welcome to the mtg card api\n")
@@ -9,7 +10,7 @@ app.get("/", function(req, rsp){
 
 app.get("/:cardnum?", function(req, rsp) {
 
-    db.card(req.params.cardnum, function(card){
+    db.card(req.params.cardnum, SCRAPE_DEBUG, function(card){
         if(card){
             var body = JSON.stringify(card, null, 2) + "\n"
             rsp.send(body)
