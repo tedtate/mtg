bcc1727915dc5c7508753ad5626f74abf371f82e
Ted Tate <tedtate@gmail.com>
2014-02-05 01:25:19 -0800

    Setting up redis for prod

--- serve/db.js
+++ serve/db.js
@@ -1,14 +1,22 @@
-var redis  = require("redis")
-var scrape    = require("./scrape")
-var client = redis.createClient('6379', '127.0.0.1')
+var scrape = require("./scrape");
+var redis;
+
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    redis = require('redis').createClient('6379', '127.0.0.1')
+}
 
 exports.card = function(key, debug, cb){
-    client.hgetall(key, function(err, reply){
+    redis.hgetall(key, function(err, reply){
         if (reply && !debug) {
             cb(reply)
         } else {
             scrape.card(key, function(card) {
-                client.hmset(key,
+                redis.hmset(key,
                     'cardname', card.cardname,
                     'cost', card.cost,
                     'cmc', card.cmc,
